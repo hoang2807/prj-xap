c8a737c9864883d1391aa196ff80e619e1b1e5fa
hoang2807 <viethoang28072000@gmail.com>
2022-08-25 18:09:22 +0700

    add helper

--- App.tsx
+++ App.tsx
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text, Button} from 'react-native';
+import {takePicture, listImages, oscState, status} from './src/helper';
 
 export default function App() {
-  const [info, setInfo] = useState('')
+  const [info, setInfo] = useState('');
   useEffect(() => {
     const getInfo = async () => {
       const response = await fetch('http://192.168.1.1/osc/info');
-      const body = await response.json()
-      setInfo(body)
-    }
+      const body = await response.json();
+      setInfo(body);
+      // console.log(body)
+    };
 
-    getInfo()
-      .catch(e => console.log(e))
-  }, [])
+    getInfo().catch(e => console.log(e));
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>{info}</Text>
+      <Text>{JSON.stringify(info)}</Text>
+      <View style={{marginTop: 10}} />
+      <Button onPress={takePicture} title="take picture" />
+      <View style={{marginTop: 10}} />
+      <Button onPress={oscState} title="camera state" />
+      <View style={{marginTop: 10}} />
+      <Button onPress={listImages} title="list images" />
+      <View style={{marginTop: 10}} />
+      <Button onPress={status} title="status" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    alignItems: 'center',
+  },
+});
--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,76 +1,190 @@
 function takePicture() {
-  const url = 'http://192.168.1.1/osc/commands/execute'
+  const url = 'http://192.168.1.1/osc/commands/execute';
   fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
     body: JSON.stringify({
-      'name': 'camera.takePicture'
-    })
+      'name': 'camera.takePicture',
+    }),
   })
     .then(res => {
-      console.log(`HTTP code: ${res.status}`)
-      res.json()
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
+      console.log(res.json());
     })
-    .then(data => console.log(JSON.stringify(data, null, 2)))
-    .catch(error => console.error(error))
+    // .then(data => console.log(JSON.stringify(data, null, 2)))
+    .catch(error => console.error(error));
 }
 
 function setOption(options: object) {
-  const url = 'http://192.168.1.1/osc/commands/execute'
+  const url = 'http://192.168.1.1/osc/commands/execute';
   fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
     body: JSON.stringify({
       'name': 'camera.setOptions',
       'parameters': {
-        'options': options
-      }
-    })
+        options: options,
+      },
+    }),
   })
     .then(res => {
-      console.log(`HTTP code: ${res.status}`)
-      res.json()
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
     })
     .then(data => console.log(data))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 }
 
 function getOption(options: object) {
-  const url = 'http://192.168.1.1/osc/commands/execute'
+  const url = 'http://192.168.1.1/osc/commands/execute';
   fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
     body: JSON.stringify({
       'name': 'camera.getOptions',
       'parameters': {
-        'options': options
-      }
-    })
+        options: options,
+      },
+    }),
   })
     .then(res => {
-      console.log(`HTTP code :${res.status}`)
-      res.json()
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
     })
     .then(data => console.log(data))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 }
 
-function listImages(parameters: object) {
-  const url = 'http://192.168.1.1/osc/commands/execute'
+function listImages() {
+  const url = 'http://192.168.1.1/osc/commands/execute';
   fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
     body: JSON.stringify({
       'name': 'camera.listImages',
-      'parameters': parameters
-    })
+      'parameters': {
+        fileType: 'all',
+        entryCount: 3,
+        includeThumb: false,
+      },
+    }),
   })
     .then(res => {
-      console.log(`HTTP code: ${res.status}`)
-      res.json()
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
     })
     .then(data => console.log(data))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 }
 
-export { takePicture, setOption, getOption, listImages }
+function oscState() {
+  const url = 'http://192.168.1.1/osc/state';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  })
+    .then(res => {
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+function status() {
+  const url = 'http://192.168.1.1/osc/commands/status';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  })
+    .then(res => {
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+function listAll() {
+  const url = 'http://192.168.1.1/osc/commands';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json:charset=utf-8',
+    },
+    body: JSON.stringify({
+      'name': 'camera._listAll',
+    }),
+  })
+    .then(res => {
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+function startSession() {
+  const url = 'http://192.168.1.1/osc/commands';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json:charset=utf-8',
+    },
+    body: JSON.stringify({
+      'name': 'startSession',
+      'parameters': {},
+    }),
+  })
+    .then(res => {
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+function endSession() {
+  const url = 'http://192.168.1.1/osc/commands';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json:charset=utf-8',
+    },
+    body: JSON.stringify({
+      'name': 'camera.closeSession',
+      'parameters': {},
+    }),
+  })
+    .then(res => {
+      console.log(`HTTP code: ${res.status}`);
+      res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+export {
+  takePicture,
+  setOption,
+  getOption,
+  listImages,
+  oscState,
+  status,
+  listAll,
+  startSession,
+  endSession,
+};
